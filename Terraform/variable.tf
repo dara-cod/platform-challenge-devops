c5b363fc1f2778822399b3de9a66f41737d04edc
daraildes <dara.aragao17@gmail.com>
2024-03-15 18:07:12 -0300

    Variable editado

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,3 +1,7 @@
 variable "region" {}
 variable "ami" {}
 variable "instance_type" {}
+#variable "key_name" {}#
+#variable "vpc_security_group_ids" {}#
+variable "subnet_id" {}
+variable "ssh_cidr_range" {}
